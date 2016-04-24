6f08484cefca8e2646325c4f61ffa19ec8da78e7
cole1717 <bush.c@husky.neu.edu>
2016-04-24 13:45:48 -0400

    Update before switch to pulsesink

--- spectrum.cpp
+++ spectrum.cpp
@@ -210,8 +210,8 @@ int main(int argc, char** argv)
 
     init(argc, argv);
     SpectrumAnalyzer player;
-    player.addCanvas(canvas);
-    player.setBepis(canvas);
+    //player.addCanvas(canvas);
+    //player.setBepis(canvas);
 
     try
     {
